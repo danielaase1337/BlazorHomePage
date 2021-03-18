49a0b366d689d368b583833f90903a7d59f57852
danielaase1337 <daniel.aase@focus.no>
2021-03-18 10:53:30 +0100

    Litt småting for å få alt til å fungerer. Hoster server api på azure i stede for i netlify

--- BlazorHomepage/Client/Program.cs
+++ BlazorHomepage/Client/Program.cs
@@ -35,7 +35,14 @@ namespace BlazorHomepage.Client
 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://blazor-supergnisten-api.azurewebsites.net/") });
+
+#if Debug
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+#else
+                             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://blazor-supergnisten-api.azurewebsites.net/") });
+#endif
+
             builder.Services.AddOidcAuthentication(options =>
             {
                 builder.Configuration.Bind("Auth0", options.ProviderOptions);
